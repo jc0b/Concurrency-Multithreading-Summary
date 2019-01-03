3a152eb04e8ca32d7d5396f6f147b0f789ff2fd4
Jacob Burley <j.s.burley@student.vu.nl>
2019-01-03 19:20:13 +0100

    modified ftp deploy sscript

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ for f in _build/*
 	if [ "$f" == "main.pdf" ]
 	then
  		echo "Uploading $f"
- 		curl --ftp-create-dirs -T $f -u $USERNAME:$PASSWORD ftp://ftp.jacobsburley.com/files.jacobsburley.com/$f
+ 		curl -v --ftp-create-dirs -m 60 -T $f -u $USERNAME:$PASSWORD ftp://ftp.jacobsburley.com/files.jacobsburley.com/$f
 	fi
 done
 
