a6cb0e256249d42b180c36921e67578b72678fdb
Jacob Burley <j.s.burley@student.vu.nl>
2019-01-03 12:19:34 +0100

    fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 for f in _build/*
   do
 	#only upload this one file
-	if [ "$f" == "_build/main.pdf" ]
+	if [ "$f" == "main.pdf" ]
 	then
  		echo "Uploading $f"
  		curl --ftp-create-dirs -T $f -u $USERNAME:$PASSWORD ftp://ftp.jacobsburley.com/files.jacobsburley.com/$f
