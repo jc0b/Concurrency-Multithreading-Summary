a18005c2e2e1d8238de99071551ca1970153faa6
Jacob Burley <j.s.burley@student.vu.nl>
2019-01-03 12:12:57 +0100

    deploys to ftp now

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for f in _build/*
+  do
+	#only upload this one file
+	if [ "$f" == "_build/main.pdf" ]
+	then
+ 		echo "Uploading $f"
+ 		curl --ftp-create-dirs -T $f -u $USERNAME:$PASSWORD ftp://ftp.jacobsburley.com/files.jacobsburley.com/$f
+	fi
+done
+
